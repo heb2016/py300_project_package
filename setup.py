da67cd5a86e32e563ac0293ea357ee85d75615d0
He, Beatrice <beatrice.he@cdk.com>
2017-04-19 18:08:17 -0700

    intital commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+setup(
+    name='py300-project-package',
+    packages=[],
+    version='0.1.0',
+    description='Heb6 Py300 Project for a Python package',
+    author='Beatrice He',
+    license='BSD',
+    author_email='beatrice.he@cdk.com',
+    url='https://github.com/heb6/py300_project_package',
+    keywords=['py300', 'cdk', 'package', ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development',
+    ],
+)
